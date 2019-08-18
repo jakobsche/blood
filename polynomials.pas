74ea133d57b64afd285ea9cecaf666c80e7cfaba
jakobsche <messages@jakobsche.de>
2019-08-18 18:24:44 +0200

    continued TPolynomial implementation

--- polynomials.pas
+++ polynomials.pas
@@ -17,17 +17,22 @@ type
   private
     FArgument: Extended;
     FCoefficients: TCoefficientArray;
+    FDerivation: TPolynomial;
     function GetCoefficients(AnIndex: Integer): Integer;
     function GetDegree: Integer;
+    function GetDerivation: TPolynomial;
     function GetDerivations(ANumber: Integer): Extended;
     function GetValue: Extended;
+    procedure SetArgument(AValue: Extended);
     procedure SetCoefficients(AnIndex: Integer; AValue: Integer);
     procedure SetDegree(AValue: Integer);
+    property Derivation: TPolynomial read GetDerivation;
   public
     constructor Create;
     destructor Destroy; override;
-    property Argumet: Extended read FArgument write FArgument;
-    property Coefficients[AnIndex: Integer]: Integer read GetCoefficients write SetCoefficients;
+    property Argument: Extended read FArgument write SetArgument;
+    property Coefficients[AnIndex: Integer]: Integer read GetCoefficients
+      write SetCoefficients;
     property Degree: Integer read GetDegree write SetDegree;
     property Derivations[ANumber: Integer]: Extended read GetDerivations;
     property Value: Extended read GetValue;
@@ -36,6 +41,20 @@ type
 implementation
 
 { TPolynomial }
+
+function TPolynomial.GetDerivation: TPolynomial;
+var
+  i: Integer;
+begin
+  if not Assigned(FDerivation) then begin
+    FDerivation := TPolynomial.Create;
+    FDerivation.Degree := Degree - 1;
+    for i := FDerivation.Degree downto 0 do
+      FDerivation.Coefficients[i] := (i + 1) * Coefficients[i + 1];
+  end;
+  FDerivation.Argument := Argument;
+  Result := FDerivation
+end;
 
 function TPolynomial.GetCoefficients(AnIndex: Integer): Integer;
 begin
@@ -48,8 +67,20 @@ begin
 end;
 
 function TPolynomial.GetDerivations(ANumber: Integer): Extended;
+var
+  i: Integer;
+  x: TPolynomial;
 begin
-  Result := 0;
+  if ANumber > Degree then Result := 0
+  else begin
+    i := 0;
+    x := Self;
+    while i < ANumber do begin
+      x := x.Derivation;
+      Inc(i)
+    end;
+    Result := x.Value
+  end;
 end;
 
 function TPolynomial.GetValue: Extended;
@@ -57,25 +88,36 @@ begin
  Result := 0;
 end;
 
+procedure TPolynomial.SetArgument(AValue: Extended);
+begin
+  if FArgument = AValue then Exit;
+  FArgument := AValue;
+end;
+
 procedure TPolynomial.SetCoefficients(AnIndex: Integer; AValue: Integer);
 begin
-  FCoefficients[AnIndex] := AValue
+  FCoefficients[AnIndex] := AValue;
+  if Assigned(FDerivation) then FreeAndNil(FDerivation);
 end;
 
 procedure TPolynomial.SetDegree(AValue: Integer);
 begin
-  SetLength(FCoefficients, AValue + 1)
+  if AValue <> GetDegree then begin
+    SetLength(FCoefficients, AValue + 1);
+    if Assigned(FDerivation) then FreeAndNil(FDerivation)
+  end
 end;
 
 constructor TPolynomial.Create;
 begin
   inherited Create;
-  SetLength(FCoefficients, 1)
+  Degree := 0
 end;
 
 destructor TPolynomial.Destroy;
 begin
   SetLength(FCoefficients, 0);
+  FDerivation.Free;
   inherited Destroy;
 end;
 
